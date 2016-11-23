3a2c96c4621c4a1836e3d7c021d4bc841cf3d354
milly <milly@valeria>
2016-11-23 18:34:38 -0200

    alterado classe apostador

--- src/main/java/bolaoweb/controller/ApostadorBEAN.java
+++ src/main/java/bolaoweb/controller/ApostadorBEAN.java
@@ -11,6 +11,7 @@ import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
+
 public class ApostadorBEAN {
 
     private Apostador apostador = new Apostador();
@@ -59,7 +60,7 @@ public class ApostadorBEAN {
         }
         return true;
     }
-
+      //as classes não estão mais setadas como null
     public String inserirApostador() {
         apostadorDAO.inserirApostador(apostador);
         return "consulta_apostador";
